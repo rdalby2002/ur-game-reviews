f6548cdc647bd84a3da673903e4bd9f9b4e80ebd
rdalby2002 <ryancdalby@gmail.com>
2023-03-27 20:13:57 -0500

    created User model with all required fields, except it currently references blank Game.js, will fill out once I know more about IGDB API

---
+++ server/models/User.js
@@ -0,0 +1,61 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// Import schema from Game.js
+const gameSchema = require('./Game');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    savedGames: [gameSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// hash user passwords
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare and validate password on log in
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+// When a user is queried, we also get a field called "gameCount" with the number of saved games they have
+userSchema.virtual('gameCount').get(function () {
+  return this.savedGames.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
