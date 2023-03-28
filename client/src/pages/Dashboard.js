20eec12e4ea085ea7d47ba26bbe50c55979d1b26
Luis <hairchevere@gmail.com>
2023-03-27 21:36:36 -0500

    created dashboard function

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Section from '../components/Section';
+
+function Dashboard() {
+    return ( 
+        <main className="">
+            <div> <Navbar /> </div>
+            <di> <Section /></di>
+        </main>
+     );
+}
+
+export default Dashboard;
