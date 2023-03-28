486e64a52e6bd1ce433226426f09a43dfcaba0ea
Luis <hairchevere@gmail.com>
2023-03-27 21:47:10 -0500

    updated home function

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import Header from '../components/Header';
-import Navbar from '../components/Navbar';
 import Section from '../components/Section';
-import Footer from '../components/Footer';
-
-import NewReleases from './NewReleases';
-import TopRated from './TopRated'
 
 
 
 
-function App() {
+function Home() {
   return (
     <main className="">
-      <div> <Header /> </div>
-      <div> <Navbar /> </div>
       <div> <Section /> </div>
-      <div> <Footer /> </div>
     </main>
   );
 }
 
-export default App;
+export default Home;
 
