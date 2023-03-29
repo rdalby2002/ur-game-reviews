95f442a452f8e8969266cf6f8bc0f86966da7aed
CASTICRI003 <ccastillo0924@gmail.com>
2023-03-28 19:55:43 -0500

    deleted api, put api data into auth, set up server

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,51 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models/User');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        users: async () => {
+            return User.find();
+        },
+
+        user: async (parent, { userId }) => {
+            return User.findOne({ _id: userId });
+        },
+
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return Profile.findOne({_id: context.user.id });
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        // here will go game requests
+    },
+
+    Mutation: {
+        addUser: async (parent, { name, email, password }) => {
+            const profile = await User.create({ name, email, password });
+            const token = signToken(profile);
+
+            return { token, profile };
+        },
+        login: async (parent, { email, password }) => {
+            const profile = await User.findOne({ email });
+
+            if (!profile) {
+                throw new AuthenticationError('Credentials are incorrect!');
+            }
+
+            const correctPw = await profile.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Credentials are incorrect!');
+            }
+
+            const token = signToken(profile);
+            return { token, profile };
+        },
+
+    }
+};
+
+module.exports = resolvers;
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-
+import apicalypse from 'apicalypse';
 const secret = 'mysecretssshhhhhhh';
 const expiration = '2h';
 
@@ -24,8 +24,37 @@ module.exports = {
 
         return req;
     },
-    signToken: function ({ email, name, _id }) {
-        const payload = { email, name, _id };
+    signToken: function ({ email, name, password }) {
+        const payload = { email, name, password };
         return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
     },
+    igdbRequest:  async function (token) {
+        const requestOptions = {
+            queryMethod: 'url',
+            // method might have to be get?
+            method: 'post',
+            baseURL: 'https://api.igdb.com/v4',
+            headers: {
+                'Accept': 'application/json',
+                'Client-ID': 'i98gt92fpw1mxgt5x39ffm47s3ehr1',
+                'Authorization': 'Bearer 8haqod3b087geqce4c1oteg8q8wyxr'
+            },
+            responseType: 'json',
+            timeout: 1000,
+            auth: {
+            email: token.email,
+            password: token.password
+            },
+        };
+        
+        // will need to really work on getting this api request right
+        const response = await apicalypse(requestOptions)
+        // may need to use slug as well as name in case we use em for URL requests
+        .fields(['id,name,first_release_date,cover,rating,summary'])
+        .sort('name', 'desc')
+        .search(`${game}`)
+        .request('/games');
+        
+        return response 
+    }
 };
