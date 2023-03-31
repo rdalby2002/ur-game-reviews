1db68b488501f302e9d3603e9ee41db7e7d8844a
Luis <hairchevere@gmail.com>
2023-03-30 22:19:59 -0500

    created social icons

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { SocialIcon } from 'react-social-icons'
 
 function Footer() {
   return (
-    <footer id="footer">
-       <div className="">
-          <p className="">
-          <FontAwesomeIcon icon="fa-brands fa-instagram" href=""/>
-          <FontAwesomeIcon icon="fa-brands fa-twitter" href=""/>
-          <FontAwesomeIcon icon="fa-brands fa-twitch" href="" />
+    <footer>
+       <div className="center">
+          <p className="social">
+          <SocialIcon url="https://twitter.com/" />
+          <SocialIcon url="https://www.instagram.com/" />
+          <SocialIcon url="https://www.twitch.com/" />
        </p>
       </div>
     </footer>
