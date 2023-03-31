134417360be9367dadc8196895c2e57694b3bae5
Luis <hairchevere@gmail.com>
2023-03-30 19:27:57 -0500

    redone Navbar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,65 +1,19 @@
 import React from "react";
 // import icons
-import 'antd/dist/reset.css';
-import { Layout } from 'antd';
 import { useState } from "react";
 // import { Auth } from '../utils/Auth';
-const { Sider } = Layout;
+import Nav from 'react-bootstrap/Nav';
 
-function Navbar(handlePageChange, currentPage) {
-    const [collapsed, setCollapsed] = useState(false);
-
-  // const logout = (event) => {
-  //   event.preventDefault();
-  //   Auth.logout();
-  // };
+function Navbar() {
   return (
-    <Layout>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo" />
-        <li className="nav-item">
-        <a
-          href="#home"
-          onClick={() => handlePageChange('Home')}
-          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-        >
-          Home
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#topRated"
-          onClick={() => handlePageChange('topRated')}
-          // Check to see if the currentPage is `topRated`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'topRated' ? 'nav-link active' : 'nav-link'}
-        > ⇠Top Rated
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#newReleases"
-          onClick={() => handlePageChange('newReleases')}
-          // Check to see if the currentPage is `newReleases`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'newReleases' ? 'nav-link active' : 'nav-link'}
-        >
-          ⇡New Releases
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#logout"
-          onClick={() => handlePageChange('Logout')}
-          // Check to see if the currentPage is `Logout`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Logout' ? 'nav-link active' : 'nav-link'}
-        >
-          ⇢Logout
-        </a>
-      </li>
-      </Sider>
-      </Layout>
-  )
+    <Nav defaultActiveKey="/home" className="flex-column">
+      <Nav.Link href="/home">Active</Nav.Link>
+      <Nav.Link eventKey="/toprated">⇠Top Rated</Nav.Link>
+      <Nav.Link eventKey="/newreleases">⇡New Releases</Nav.Link>
+      <Nav.Link eventKey="/favorites">⇢Favorites</Nav.Link>
+      <Nav.Link eventKey="/logout">⇣Logout</Nav.Link>
+    </Nav>
+  );
 }
 
 export default Navbar;
