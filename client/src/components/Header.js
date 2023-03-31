59f53b40090bb1ba6654558302105a5695fc9141
Luis <hairchevere@gmail.com>
2023-03-30 19:26:42 -0500

    pathed image for header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Logo from '../assets/images/urglogo.png'
 
 function Header() {
  return (
     <header>
-        <img alt="Logo" src="./urglogo.png"></img>
+        <img alt="Logo" src={Logo}></img>
     </header>
  )
 }
