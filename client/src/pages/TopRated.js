a29ed323fed26b276a2e840a54943e81f9de937e
Luis <hairchevere@gmail.com>
2023-03-30 19:25:00 -0500

    redid entire component

--- client/src/pages/TopRated.js
+++ client/src/pages/TopRated.js
@@ -1,16 +1,38 @@
 import React from "react";
-const { Card } = 'Layout';
+import { QUERY_NEW } from "../utils/queries";
+import Auth from '../utils/Auth';
+import { useQuery } from '@apollo/client';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-function TopRated(){
-    <Card
-    hoverable
-    style={{
-      width: 240,
-    }}
-    cover={<img alt="NewReleases" />}
-  >
+function TopRated() {
 
-  </Card>
-};
+  const { loading, data } = useQuery(QUERY_NEW);
+  const topRated = data?.topRated || [];
+
+  const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+  if (!token) {
+    return false;
+  }
+
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
+  
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top">{topRated}</Card.Img>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+}
 
 export default TopRated;
