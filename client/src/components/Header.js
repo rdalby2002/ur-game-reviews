cce190fe409d91f953c89946f95698c27762d6e8
Luis <hairchevere@gmail.com>
2023-03-30 20:57:51 -0500

    added logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,9 @@ import Logo from '../assets/images/urglogo.png'
 function Header() {
  return (
     <header>
+        <div className="center">
         <img alt="Logo" src={Logo}></img>
+        </div>
     </header>
  )
 }
