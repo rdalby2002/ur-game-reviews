6a8e3a15d0e382d6a11c8c6de753a6527a2b4bc7
Luis <hairchevere@gmail.com>
2023-04-02 17:10:25 -0500

    added temp slides. pathed login and sign up buttons

--- client/src/components/Section.js
+++ client/src/components/Section.js
@@ -1,16 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import { Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import Auth from '../utils/Auth';
+import { useNavigate} from 'react-router-dom';
 
 // import { QUERY_MAIN } from '../utils/queries';
 
 function Section() {
-  const logout = (event) => {
-    event.preventDefault();
-    Auth.logout();
-  };
+  const navigate = useNavigate();
+
   // const { loading, data } = useQuery(QUERY_MAIN);
   // const main = data?.main || [];
   const main = {
@@ -39,62 +36,46 @@ function Section() {
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src={main.games.cover}
+              src={main.games}
               alt="First slide"
             />
-            <Carousel.Caption>
-              <h3>{main.games.title}</h3>
-              <p>{main.games.rating}</p>
-            </Carousel.Caption>
           </Carousel.Item>
 
-          {/* <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={cover}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>{title}</h3>
-          <p>{rating}</p>
-        </Carousel.Caption>
-      </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={main.games}
+              alt="second slide"
+            />
+          </Carousel.Item>
 
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={cover}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>{title}</h3>
-          <p>{rating}</p>
-        </Carousel.Caption>
-      </Carousel.Item> */}
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={main.games}
+              alt="third slide"
+            />
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={main.games}
+              alt="fourth slide"
+            />
+          </Carousel.Item>
 
         </Carousel>
         <div className='info'>
+          <div>
           <p>
             Are you a Twitch streamer looking to showcase the best or newest in the gaming industry? Then look no further than <b>Ur Game Reviews</b>. We are your one stop shop for the latest and greatest! Join our Community to see what the buzz is all about!
           </p>
+          </div>
           <div>
-          {Auth.loggedIn() ? (
-            <>
-              <span>Hey there, {Auth.getProfile().data.username}!</span>
-              <button className="btn btn-lg btn-light m-2" onClick={logout}>
-                Logout
-              </button>
-              </>
-          ):(
-            <>
-            <Link>
-          <Button variant='dark' to='/login'>Login</Button>
-            </Link>
-            <Link>
-          <Button variant='dark' to='/signup'>Sign up</Button>
-            </Link>
-            </>
-          )}
+          <Button variant='dark' onClick={() => navigate("/login")}>Login</Button>
+          {' '}  
+          <Button variant='dark' onClick={() => navigate("/signup")}>Sign up</Button>
             </div>
         </div>  
       </div>
