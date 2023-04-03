2d4e14a318655d41fa6394b9cbd5384a451fe355
Luis <hairchevere@gmail.com>
2023-04-02 19:09:46 -0500

    added handlesubmit and text input consts

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,72 +1,74 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 import { useNavigate} from 'react-router-dom';
-import React, { useState } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/Auth';
 
-const LoginForm = () => {
-  const [userFormData, setUserFormData] = useState({ username: '', password: '' });
-  const [validated] = useState(false);
-  const [showAlert, setShowAlert] = useState(false);
-
-  const [login] = useMutation(LOGIN_USER);
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
-  };
-
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-
-    try {
-      const { data } = await login({
-        variables: { ...userFormData }
-      });
-
-      Auth.login(data.login.token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
-
-    setUserFormData({
-      username: '',
-      password: '',
-    });
-  };
 
 function LoginForm() {
   const navigate = useNavigate();
-  
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const CheckTextInput = (event) => {
+    const blurredEl = event.target.getAttribute("id");
+
+    if (blurredEl === "username") {
+      // error
+      alert('Please enter a username!')
+      return;
+    } else if (blurredEl === "password") {
+      // error
+      alert('Please enter your password!')
+      return;
+    }
+
+    //Checked Successfully
+    //Do whatever you want
+    alert('Success');
+  };
+
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if ( username === '' || password === '') {
+      // error
+      alert("Please input requested information above!")
+      return;
+    }
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "test", username, password }),
+    })
+      .then(() => alert("Message sent!"))
+      .catch((error) => alert(error));
+  }
+
   return (
     <div>
 
-    <Form
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-    >
+    <Form>
 
-      <Form.Group className="mb-3" controlId="username">
+      <Form.Group onSubmit={handleSubmit} className="mb-3" controlId="username">
         <Form.Label>Username</Form.Label>
-        <Form.Control type="username" placeholder="username" />
+        <Form.Control type="username" placeholder="username" onBlur={CheckTextInput} onChange={(e) => setUsername(e.target.value)} />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group onSubmit={handleSubmit} className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control  type="password" placeholder="password" onBlur={CheckTextInput} onChange={(e) => setPassword(e.target.value)} />
       </Form.Group>
 
       <Button variant="dark" type="submit" onClick={() => navigate("/dashboard")}>
@@ -77,45 +79,7 @@ function LoginForm() {
     </Form>
     </div>
 
-  return (
-    <>
-      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-          Something went wrong with your login credentials!
-        </Alert>
-        <Form.Group className='col-sm-6'>
-          <Form.Label htmlFor='username'>Username</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Your username'
-            name='username'
-            onChange={handleInputChange}
-            value={userFormData.username}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Username is required!</Form.Control.Feedback>
-        </Form.Group>
-
-        <Form.Group className='col-sm-6'>
-          <Form.Label htmlFor='password'>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Your password'
-            name='password'
-            onChange={handleInputChange}
-            value={userFormData.password}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-        </Form.Group>
-        <Button
-          disabled={!(userFormData.username && userFormData.password)}
-          type='submit'
-          variant='dark'>
-          Submit
-        </Button>
-      </Form>
-    </>
+ 
   );
 };
 
