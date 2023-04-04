20fbfbeeeeba09665ce86d957abf538aea8f2352
Luis <hairchevere@gmail.com>
2023-04-04 13:29:10 -0500

    game data

---
+++ client/src/data.js
@@ -0,0 +1,97 @@
+export const carouselGames = [
+    {
+        name: "Yakuza: Like A Dragon",
+        cover: "https://cdn.cloudflare.steamstatic.com/steam/apps/1235140/capsule_616x353.jpg?t=1677167527"
+    },
+    {
+        name: "Death Stranding",
+        cover: "https://cdn1.epicgames.com/f4a904fcef2447439c35c4e6457f3027/offer/DS_wide-2560x1440-c3d7bbf8ee36dd025610088381a5235a.jpg"
+    },
+    {
+        name: "Cyberpunk 2077",
+        cover: "https://cdn1.epicgames.com/offer/77f2b98e2cef40c8a7437518bf420e47/EGS_Cyberpunk2077_CDPROJEKTRED_S1_03_2560x1440-359e77d3cd0a40aebf3bbc130d14c5c7"
+    },
+    {
+        name: "Persona 5 Royal",
+        cover: "https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/en_US/games/switch/p/persona-5-royal-switch/hero"
+    }
+]
+
+export const topGames = [
+    {
+        name: "The Witcher 3: The Wild Hunt",
+        rating: "95%",
+        cover: "https://image.api.playstation.com/vulcan/ap/rnd/202211/0914/TvcIHkYqqln1RGbaFqBeuFp6.jpg?w=440",
+        summary: "The Witcher 3: Wild Hunt is a 2015 action role-playing game developed and published by CD Projekt. It is the sequel to the 2011 game The Witcher 2: Assassins of Kings and the third game in The Witcher video game series, played in an open world with a third-person perspective.",
+    },
+    {
+        name: "Hollow Knight",
+        rating: "94%",
+        cover: "https://cdn.cloudflare.steamstatic.com/steam/apps/367520/capsule_616x353.jpg?t=1667006028",
+        summary: "Hollow Knight is a challenging 2D action-adventure. You'll explore twisting caverns, battle tainted creatures and escape intricate traps, all to solve an ancient long-hidden mystery. Explore vast, interconnected worlds. Encounter a bizarre collection of friends and foes. Evolve with powerful new skills and abilities.",
+    },
+    {
+        name: "Grand Theft Auto IV",
+        rating: "98%",
+        cover: "https://assets1.ignimgs.com/2018/10/08/grand-theft-auto-4-and-all-dlc-now-playable-on-xbox-one-kgw7-1539032764777_160w.jpg?width=1280",
+        summary: "Grand Theft Auto IV is a 2008 action-adventure game developed by Rockstar North and published by Rockstar Games. It is the sixth main entry in the Grand Theft Auto series, following 2004's Grand Theft Auto: San Andreas, and the eleventh instalment overall.",
+    },
+    {
+        name: "The Last Of Us",
+        rating: "97%",
+        cover: "https://upload.wikimedia.org/wikipedia/en/4/46/Video_Game_Cover_-_The_Last_of_Us.jpg",
+        summary: "The Last of Us is an action-adventure survival horror game franchise created by Naughty Dog and Sony Interactive Entertainment. The series is set in a post-apocalyptic United States ravaged by cannibalistic creatures infected by a mutated fungus in the genus Cordyceps.",
+    }
+    ,{
+        name: "God Of War",
+        rating: "94%",
+        cover: "https://image.api.playstation.com/vulcan/ap/rnd/202207/1210/8jHGChKiNVguguezuyE35X2n.png",
+        summary: "God of War Ragnarök is an action-adventure game developed by Santa Monica Studio and published by Sony Interactive Entertainment. It was released worldwide on November 9, 2022, for both the PlayStation 4 and PlayStation 5, marking the first cross-gen release in the God of War series.",
+    },
+    {
+        name: "Elder Ring",
+        rating: "92%",
+        cover: "https://static.bandainamcoent.eu/high/elden-ring/elden-ring/00-page-setup/elden-ring-new-header-mobile.jpg",
+        summary: "Elden Ring is a 2022 action role-playing game developed by FromSoftware and published by Bandai Namco Entertainment. Directed by Hidetaka Miyazaki with worldbuilding provided by fantasy writer George R. R. Martin, it was released for PlayStation 4, PlayStation 5, Windows, Xbox One, and Xbox Series X/S on February 25.",
+    }
+
+]
+
+export const newGames = [
+    {
+        name: "Resident Evil 4",
+        date: "March 23, 2023",
+        cover: "https://img.youtube.com/vi/Id2EaldBaWw/maxresdefault.jpg",
+        summary: "Resident Evil 4 is a 2023 survival horror game developed and published by Capcom. It is a remake of the 2005 game Resident Evil 4. Players control the US agent Leon S. Kennedy, who must save Ashley Graham, the daughter of the United States president, from the mysterious Los Iluminados cult."
+    },
+    {
+        name: "Hi-Fi Rush",
+        date: "January 25,2023",
+        cover: "https://i0.wp.com/news.xbox.com/en-us/wp-content/uploads/sites/2/2023/01/Hibiki-News-Hero-Image-4916fb477b67e704ab35.jpg?resize=1200%2C675&ssl=1",
+        summary: "Hi-Fi Rush is a rhythm-based action game developed by Tango Gameworks and published by Bethesda Softworks for the Windows and Xbox Series X/S platforms. It was announced on January 25, 2023 and released worldwide the same day."
+    },
+    {
+        name: "Dredge",
+        date: " March 30, 2023",
+        cover: "https://cdn.akamai.steamstatic.com/steam/apps/1562430/header.jpg?t=1680256832",
+        summary: "DREDGE is a single-player fishing adventure with a sinister undercurrent. Sell your catch, upgrade your boat, and dredge the depths for long-buried secrets. Explore a mysterious archipelago and discover why some things are best left forgotten."
+    },
+    {
+        name: "Hogwarts Legacy",
+        date: "February 10, 2023",
+        cover: "https://cdn1.epicgames.com/offer/e97659b501af4e3981d5430dad170911/EGS_HogwartsLegacy_AvalancheSoftware_S1_2560x1440-2baf3188eb3c1aa248bcc1af6a927b7e",
+        summary: "Hogwarts Legacy is a 2023 action role-playing game developed by Avalanche Software and published by Warner Bros. Games under its Portkey Games label. The game is set in the Wizarding World universe, taking place in the late 1800s, a century prior to the events chronicled in the Harry Potter novels."
+    },
+    {
+        name: "Wo Long: Fallen Dynasty",
+        date: "March 30, 2023",
+        cover: "https://teamninja-studio.com/wolong/assets/img/og.jpg",
+        summary: "Wo Long: Fallen Dynasty is a historical fantasy action role-playing video game developed by Team Ninja and published by Koei Tecmo. The game was released on March 3, 2023 on PlayStation 4, PlayStation 5, Windows, Xbox One and Xbox Series X/S."
+    },
+    {
+        name: "Atomic Heart",
+        date: "February 21, 2023",
+        cover: "https://cdn.akamai.steamstatic.com/steam/apps/668580/capsule_616x353.jpg?t=1676995676",
+        summary: "Atomic Heart is a single-player first-person shooter developed by Mundfish and published by Focus Entertainment and 4Divinity. The game was released for the PlayStation 4, PlayStation 5, Windows, Xbox One, and Xbox Series X/S on February 21, 2023. The game received generally positive reviews upon release."
+    },
+]
